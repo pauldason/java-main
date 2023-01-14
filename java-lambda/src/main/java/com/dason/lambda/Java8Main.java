1037b101edac25c699d3368f1b425e326c559174
dsfu <pauldason@vip.qq.com>
2023-01-14 21:46:45 +0800

    提交 revers 备注为 liyu haokeai

---
+++ java-lambda/src/main/java/com/dason/lambda/Java8Main.java
@@ -0,0 +1,58 @@
+package com.dason.lambda;
+
+public class Java8Main {
+
+    public static void main(String args[]){
+
+        Java8Main tester = new Java8Main();
+
+        // 类型声明
+        MathOperation addition = (int a, int b) -> a + b;
+
+        // 不用类型声明
+        MathOperation subtraction = (a, b) -> a - b;
+
+        // 大括号中的返回语句
+        MathOperation multiplication = (int a, int b) -> { return a * b; };
+
+        // 没有大括号及返回语句
+        MathOperation division = (int a, int b) -> a / b;
+
+        System.out.println("10 + 5 = " + tester.operate(10, 5, addition));
+        System.out.println("10 - 5 = " + tester.operate(10, 5, subtraction));
+        System.out.println("10 x 5 = " + tester.operate(10, 5, multiplication));
+        System.out.println("10 / 5 = " + tester.operate(10, 5, division));
+
+        // 不用括号
+        GreetingService greetService1 = message ->
+                System.out.println("Hello " + message);
+
+        // 用括号
+        GreetingService greetService2 = (message) ->
+                System.out.println("Hello " + message);
+
+
+        // 用括号
+        GreetingService greetService3 = (message) -> {
+            System.out.println("Hello 用括号1" + message);
+            System.out.println("Hello 用括号2" + message);
+            System.out.println("Hello 用括号3" + message);
+        };
+
+        greetService1.sayMessage("Runoob");
+        greetService2.sayMessage("Google");
+        greetService3.sayMessage("dason");
+    }
+
+    interface MathOperation {
+        int operation(int a, int b);
+    }
+
+    interface GreetingService {
+        void sayMessage(String message);
+    }
+
+    private int operate(int a, int b, MathOperation mathOperation){
+        return mathOperation.operation(a, b);
+    }
+}
---
+++ java-lambda/src/main/java/com/dason/lambda/lambdaMain.java
@@ -0,0 +1,20 @@
+package com.dason.lambda;
+
+public class lambdaMain {
+
+    public static void main(String[] args) {
+
+        //String parms = "parms";
+
+        new Thread( () -> System.out.println("In Java8, Lambda expression rocks !!") ).start();
+
+
+
+
+
+
+
+
+
+    }
+}
